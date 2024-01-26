d4cd83729e92fd04de5414cf57ef035a1298c23d
Severin Messenbrink <severin@messenbrink.eu>
2024-01-26 13:27:03 +0100

    wip

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -6,12 +6,6 @@
 }: {
   imports = [./hardware-configuration.nix];
 
-  age.secrets.spotify = {
-    file = "${self}/secrets/spotify.age";
-    owner = "mihai";
-    group = "users";
-  };
-
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda/";
   boot.loader.grub.useOSProber = true;
