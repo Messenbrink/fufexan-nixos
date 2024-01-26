9828428ffbea603f4eafe52d2d0cad0a55400304
Severin Messenbrink <severin@messenbrink.eu>
2024-01-26 14:20:48 +0100

    wip

--- home/specialisations.nix
+++ home/specialisations.nix
@@ -28,20 +28,20 @@
 
   theme = {
     # specific to unsplash
-    # wallpaper = let
-    #   params = "?q=85&fm=jpg&crop=fit&cs=srgb&w=2560";
-    #   url = "https://images.unsplash.com/photo-1608507974219-2df72d775da0${params}.jpg";
-    #   sha256 = "00hj6svcfm969h0fmf5hfmgr8sa9vm789dsl0nzdfqpc1mqy7h2";
-    #   ext = lib.last (lib.splitString "." url);
-    # in
-    #   builtins.fetchurl {
-    #     name = "wallpaper-${sha256}.${ext}";
-    #     inherit url sha256;
-    #   };
+    wallpaper = let
+      params = "?q=85&fm=jpg&crop=fit&cs=srgb&w=2560";
+      url = "https://images.unsplash.com/photo-1608507974219-2df72d775da0${params}.jpg";
+      sha256 = "00hj6svcfm969h0fmf5hfmgr8sa9vm789dsl0nzdfqpc1mqy7h2";
+      ext = lib.last (lib.splitString "." url);
+    in
+      builtins.fetchurl {
+        name = "wallpaper-${sha256}.${ext}";
+        inherit url sha256;
+      };
   };
 
   programs.matugen = {
     enable = false;
-    #wallpaper = config.theme.wallpaper;
+    wallpaper = config.theme.wallpaper;
   };
 }
